50639790baff9caceeb23820f3592b177802fde1
rishaanksirpaul <rishaanksirpaul7@gmail.com>
2021-10-10 12:52:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from pywebio.input import *
+from pywebio.output import *
+from functools import partial
+# anything = input(label = "Enter anything: ", required = True, placeholder = 'Write anything here!')
+# put_text(anything)
+
+# textarea(label = 'this is a text area',rows = 20,code={
+#     'mode': "python",
+#     'theme': 'darcula'
+#     })
+
+# put_table([
+#     [span('Name',row=2),span('Address',col=2)],
+#     ['City', 'Country'],
+#     ['Wang', 'Beijing', 'China'],
+#     ['Liu', 'New York', 'America'],
+
+# ])
+# def test(btnName,c,d):
+#     put_text('I am test function',c+d)
+
+# def button():
+#     a = 10
+#     b = 20
+#     put_buttons(['Click me'], onclick=partial(test,a,b))
+
+# button()
+
+# put_info("Info",closable= True,)
+# TEXT BUTTON
+# TEXT BUTTON
+# TEXT BUTTON
+# TEXT BUTTON
+# tasks = []
+# tasks.append(task)
+
+# def check(btnName):
+#     tasks.remove(task)
+
+
+# input(label = 'This is a date label', type = DATE, required = True)
+
+
+# List Comprehension..
+# a = [i for i in range(1,100) if i % 2 == 0]
+# print(a)
+
+def test():
+    print("Hi")
+    return ""
+
+test() # Value
+test # Reference
---
+++ tasks.py
@@ -0,0 +1,34 @@
+from pywebio.input import *
+from pywebio.output import *
+from functools import partial
+
+def onBtnPress(btnName, task_list, actual_task):
+    task_list.remove(actual_task)
+    clear('test')
+    put_table([
+        [i, put_buttons(['check'],onclick= partial(onBtnPress,task_list = task_list,actual_task = i))]for i in task_list 
+    ],
+        header= ['Task Names', 'Have you checked your tasks'],scope = 'test')
+
+
+def onGoing(btnName):
+    pass
+
+def main():
+    tasks = []
+    with use_scope('test'):
+        while True:
+            task = input(label= "Enter your tasks: ",required=True)
+            tasks.append(task)
+            clear('test')
+            put_table([
+                [i, put_buttons(['check'],onclick= partial(onBtnPress,task_list = tasks,actual_task = i))]for i in tasks 
+            ],
+             header= ['Task Names', 'Have you checked your tasks'])
+        
+# main()
+
+put_info('I am content', closable=True)
+
+# Calling by reference and calling by value..
+# 
